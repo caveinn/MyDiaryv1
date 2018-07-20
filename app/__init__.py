808ecba78a29105930aa08718b5c9b1f72b9a4f3
Kevin Nzioka <caveinncicad@gmail.com>
2018-07-20 09:11:25 +0300

    [feature #159131075] add an entry

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,46 @@
 '''intilistaion file for flask app'''
-from flask import Flask, jsonify 
+from flask import Flask, jsonify, request , make_response
 from instance.config import app_config
+import uuid
 
-entry_Id_Counter=0 #count the number of entries and use it as customer_id
-user_ID_Counter=0 #count the number of entries and use it as customer_id
-entries={1:{ "title":"example", "content":"some random stuff"}}
-users=[]
 
-def add_Entries_To_Db(title, content):
+entries={"1":{ "title":"example", "content":"some random stuff"}}
+users={"1":{"username":"jane doe", "password":"1234", "email":"janedoe@gmail.com"}}
+
+def add_Entries_To_Db(id,title, content):
     temp_Entry={}
-    entry_Id_Counter+=1
-    temp_Entry["id"]=entry_Id_Counter
     temp_Entry["title"]=title
     temp_Entry["content"]=content
-    entries.append(temp_Entry)
-
-
-
+    entries[id]=temp_Entry
 
 def create_App(config_name):
     '''Configuring the app'''
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('../instance/config.py')
+    entry_Id_Counter=0
 
     @app.route('/api/v1/entries', methods=['GET'])
     def get_All_Entries():
-        return jsonify(users)
+        '''Get all entries'''
+        return jsonify(entries)
 
     @app.route('/api/v1/entries/<entryid>', methods=['GET'])
     def get_Single_Entry(entryid):
+        '''Api endpoint to get a single entry'''
         if entryid in entries.keys():
             return jsonify({entryid:entries[entryid]})
         return jsonify({"message":"invalid id"})
+    
+    @app.route('/api/v1/entries', methods=['POST'])
+    def create_Entry():
+        data=request.get_json()
+        if data:
+            add_Entries_To_Db(str(uuid.uuid4()), data['title'], data['content'])
+            response=jsonify({"message": "entry created succesfully"})
+            response.status_code=201
+            return response
+        return jsonify({"message":"send data"})
 
 
     return app
--- test_Api.py
+++ test_Api.py
@@ -11,19 +11,20 @@ class ApiTestCase(unittest.TestCase):
         '''Initilise and setup the app'''
         self.app= create_App(config_name="testing")
         self.client= self.app.test_client 
-        self.UserData={"username":"Jane doe", "email":"janedoe@gmail.com", "password":"sometext",}
-        self.EntryData={"title": "myentry", "content":"something that doesnt really matter happened today"}
+        self.UserData=json.dumps({"username":"Jane doe", "email":"janedoe@gmail.com", "password":"sometext",})
+        self.EntryData=json.dumps({"title": "myentry", "content": "something that doesnt really matter happened today"})
     
     def test_User_Creation(self):
-        resp=self.client().post('/api/v1/user/', data=self.UserData)
+        resp=self.client().post('/api/v1/users', data=self.UserData,content_type = 'application/json')
         self.assertEqual(resp.status_code, 201)
     
     def test_Login(self):
-        resp =self.client().post('/api/v1/user/login')
+        resp =self.client().post('/api/v1/user/login', data=self.UserData)
         self.assertEqual(resp.status_code, 200)
 
     def test_Create_Entry(self):
-        resp=self.client().post('/api/v1/entry/', data=self.EntryData)
+        resp=self.client().post('/api/v1/entries', data=self.EntryData,content_type = 'application/json')
+        print(self.EntryData)
         self.assertEqual(resp.status_code, 201)
 
     def test_Get_All_Entries(self):
