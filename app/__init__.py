7bd811f42d67e6fe5f4808b7a20b762bc0381779
Kevin Nzioka <caveinncicad@gmail.com>
2018-07-20 09:22:24 +0300

    [feature #159168781 add a new user]

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,23 @@ from flask import Flask, jsonify, request , make_response
 from instance.config import app_config
 import uuid
 
-
+#data 
 entries={"1":{ "title":"example", "content":"some random stuff"}}
 users={"1":{"username":"jane doe", "password":"1234", "email":"janedoe@gmail.com"}}
 
+#functions to add new items to the data models
 def add_Entries_To_Db(id,title, content):
     temp_Entry={}
     temp_Entry["title"]=title
     temp_Entry["content"]=content
     entries[id]=temp_Entry
+
+def add_Users_To_Db(id,username, password, email):
+    temp_User={}
+    temp_User["username"]=username
+    temp_User["password"]=password
+    temp_User["email"]=email
+    users[id]=temp_User
 
 def create_App(config_name):
     '''Configuring the app'''
@@ -19,6 +27,17 @@ def create_App(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('../instance/config.py')
     entry_Id_Counter=0
+
+    @app.route('/api/v1/users', methods=['POST'])
+    def create_User():
+        '''Creating a new user'''
+        data=request.get_json()
+        if data:
+            add_Users_To_Db(str(uuid.uuid4()), data['username'], data['password'], data['email'])
+            response=jsonify({"message": "entry created succesfully"})
+            response.status_code=201
+            return response
+        return jsonify({"message":"send data"})
 
     @app.route('/api/v1/entries', methods=['GET'])
     def get_All_Entries():
